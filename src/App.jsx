475f6fa45d17e08d9831a7704a41b81d75d209ec
hyunxhri <sbulmar842@g.educaand.es>
2024-01-16 23:52:36 +0100

    changing routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Profile from './Profile'
 import LandingPage from './LandingPage'
 import Contact from './Contact'
 import updateUserLoggedInUsers from './updateUserLoggedInUsers'
-import { path } from './env/env'
 
 function App() {
 
@@ -18,22 +17,22 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={`${path}`} element={<LandingPage/>} />
-        <Route path={`${path}classes`} element={<Data endpoint="classes"/>}/>
-        <Route path={`${path}races`} element={<Data endpoint="races"/>}/>
-        <Route path={`${path}spells`} element={<SpellsPage endpoint="spells"/>} />
-        <Route path={`${path}spells/bard`} element={<SpellsPage endpoint="classes/bard/spells"/>} />
-        <Route path={`${path}spells/cleric`} element={<SpellsPage endpoint="classes/cleric/spells"/>} />
-        <Route path={`${path}spells/druid`} element={<SpellsPage endpoint="classes/druid/spells"/>} />
-        <Route path={`${path}spells/paladin`} element={<SpellsPage endpoint="classes/paladin/spells"/>} />
-        <Route path={`${path}spells/ranger`} element={<SpellsPage endpoint="classes/ranger/spells"/>} />
-        <Route path={`${path}spells/sorcerer`} element={<SpellsPage endpoint="classes/sorcerer/spells"/>} />
-        <Route path={`${path}spells/warlock`} element={<SpellsPage endpoint="classes/warlock/spells"/>} />
-        <Route path={`${path}spells/wizard`} element={<SpellsPage endpoint="classes/wizard/spells"/>} />
-        <Route path={`${path}profile`} element={<Profile/>} />
-        <Route path={`${path}contact`} element={<Contact/>} />
-        <Route path={`${path}register`} element={<RegisterForm />} />
-        <Route path={`${path}login`} element={<LoginForm />} />
+        <Route path="/" element={<LandingPage/>} />
+        <Route path="/classes" element={<Data endpoint="classes"/>}/>
+        <Route path="/races" element={<Data endpoint="races"/>}/>
+        <Route path="/spells" element={<SpellsPage endpoint="spells"/>} />
+        <Route path="/spells/bard" element={<SpellsPage endpoint="classes/bard/spells"/>} />
+        <Route path="/spells/cleric" element={<SpellsPage endpoint="classes/cleric/spells"/>} />
+        <Route path="/spells/druid" element={<SpellsPage endpoint="classes/druid/spells"/>} />
+        <Route path="/spells/paladin" element={<SpellsPage endpoint="classes/paladin/spells"/>} />
+        <Route path="/spells/ranger" element={<SpellsPage endpoint="classes/ranger/spells"/>} />
+        <Route path="/spells/sorcerer" element={<SpellsPage endpoint="classes/sorcerer/spells"/>} />
+        <Route path="/spells/warlock" element={<SpellsPage endpoint="classes/warlock/spells"/>} />
+        <Route path="/spells/wizard" element={<SpellsPage endpoint="classes/wizard/spells"/>} />
+        <Route path="/profile" element={<Profile/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/login" element={<LoginForm />} />
       </Routes>
     </BrowserRouter>
   )
